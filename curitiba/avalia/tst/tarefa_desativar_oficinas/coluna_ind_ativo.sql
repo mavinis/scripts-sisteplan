7ca9e0a5cfc650e60d01eef27258aa7b6598ab1e
mavinis <marcus.sabadin@sisteplan.com.br>
2025-04-29 17:07:05 -0300

    tarefa_desativar_oficinas

---
+++ curitiba/avalia/tst/tarefa_desativar_oficinas/coluna_ind_ativo.sql
@@ -0,0 +1,9 @@
+--- Adicionar coluna ind_ativo na tabela disciplina
+
+ALTER TABLE ppi.disciplina
+ADD COLUMN ind_ativo BOOLEAN DEFAULT TRUE;
+
+--- Setar ind_ativo das oficinas para FALSE
+UPDATE ppi.disciplina
+SET ind_ativo = FALSE
+WHERE nm_disciplina ILIKE 'Ofic%';
